ec5a4119a58538e656775b7b60cd0726976ed7c7
matteoaldeghi <maldeghi@mit.edu>
2022-02-09 17:23:12 -0500

    update

--- chemprop/features/featurization.py
+++ chemprop/features/featurization.py
@@ -577,10 +577,14 @@ class MolGraph:
                             _a2 = atom.GetIdx()
 
                 if a1 is None:
+                    smi = Chem.MolToSmiles(cm)
+                    with open('error_mol.txt',  'a') as f:
+                        f.write(smi)
                     raise ValueError(f'cannot find atom attached to [*:{r1}]')
-                if a2 is None:
-                    raise ValueError(f'cannot find atom attached to [*:{r2}]')
-                if _a2 is not None:
+                if a2 is None or _a2 is None:
+                    smi = Chem.MolToSmiles(cm)
+                    with open('error_mol.txt', 'a') as f:
+                        f.write(smi)
                     raise ValueError(f'cannot find atom attached to [*:{r2}]')
 
                 # create bond
